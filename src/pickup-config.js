aa3ae812c0497c7b41e6bff2765e07d3360d2303
Janiel777 <janiel.nunez@upr.edu>
2026-07-09 16:36:35 -0400

    Configure Firebase Auth and pickup API endpoints

--- src/pickup-config.js
+++ src/pickup-config.js
@@ -6,16 +6,20 @@ export const PICKUP_ALLOWED_ADMIN_EMAILS = [
 export const PICKUP_MOCK_MODE = false;
 
 export const PICKUP_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyDzabLj1tL_-wIyPnDiXNoNEIEfs2rT2SA",
+  authDomain: "project-fbdd8e2e-e884-43f6-81c.firebaseapp.com",
+  projectId: "project-fbdd8e2e-e884-43f6-81c",
+  appId: "1:723401665439:web:138ac596d3aa0a3eaa6baa",
 };
 
-export const PICKUP_PUBLIC_LOOKUP_URL = "";
-export const PICKUP_ADMIN_LOOKUP_URL = "";
-export const PICKUP_ADMIN_CONFIRM_URL = "";
-export const PICKUP_ADMIN_SEARCH_URL = "";
+export const PICKUP_PUBLIC_LOOKUP_URL =
+  "https://us-east1-project-fbdd8e2e-e884-43f6-81c.cloudfunctions.net/publicPickupPass";
+export const PICKUP_ADMIN_LOOKUP_URL =
+  "https://us-east1-project-fbdd8e2e-e884-43f6-81c.cloudfunctions.net/adminLookupPickupPass";
+export const PICKUP_ADMIN_CONFIRM_URL =
+  "https://us-east1-project-fbdd8e2e-e884-43f6-81c.cloudfunctions.net/adminConfirmPickup";
+export const PICKUP_ADMIN_SEARCH_URL =
+  "https://us-east1-project-fbdd8e2e-e884-43f6-81c.cloudfunctions.net/adminSearchPickupRunners";
 
 export const PICKUP_MANUAL_CONFIRM_STRATEGY = "registrationId";
 
